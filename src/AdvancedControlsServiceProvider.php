836552198f1884a27a26a6e205ff7ace4cf012dc
FX David <fxavierdavid@gmail.com>
2025-11-20 21:56:54 +0100

    Added theme toggle

--- src/AdvancedControlsServiceProvider.php
+++ src/AdvancedControlsServiceProvider.php
@@ -3,17 +3,36 @@
 namespace AllYouNeed\AdvancedControls;
  
 use Illuminate\Support\ServiceProvider;
+use AllYouNeed\AdvancedControls\Components\ThemeToggle;
  
 final class AdvancedControlsServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        // if ($this->app->runningInConsole()) {
+        if ($this->app->runningInConsole()) {
         //     $this->commands(
         //         commands: [
         //             DataTransferObjectMakeCommand::class,
         //         ],
         //     );
-        // }
+        }
+        else {
+            $this->registerComponents();
+        }   
+    }
+    public function registerComponents()
+    {
+        // // Just rename <x-icon> provided by BladeUI Icons to <x-svg> to not collide with ours
+        // Blade::component('BladeUI\Icons\Components\Icon', 'svg');
+
+        // // No matter if components has custom prefix or not,
+        // // we also register below alias to avoid naming collision,
+        // // because they are used inside some Mary's components itself.
+        // Blade::component('mary-button', Button::class);
+
+        $prefix = config('prefix');
+
+        // Blade
+        Blade::component($prefix . 'theme-toggle', ThemeToggle::class);
     }
 }
