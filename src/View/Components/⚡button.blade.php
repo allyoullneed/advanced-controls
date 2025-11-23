f9e20e280b7681254b35a5c5f7e00e6230fdab38
FX David <fxavierdavid@gmail.com>
2025-11-23 13:20:27 +0100

    Created Button component

---
+++ src/View/Components/⚡button.blade.php
@@ -0,0 +1,16 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    //
+};
+?>
+
+<button class="btn">
+    <span class="loading loading-spinner"></span>
+  
+    {{ $slot }}
+
+</button>
