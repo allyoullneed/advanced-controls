b0745709b069abbb4a3a5e29d197ccbb89ff96ed
FX David <fxavierdavid@gmail.com>
2026-01-06 22:49:58 +0100

    Improvements on Card

--- src/View/Components/Card.php
+++ src/View/Components/Card.php
@@ -8,12 +8,14 @@ use Illuminate\View\Component;
 class Card extends Component
 {
     public function __construct(
-        public mixed  $figure      = null,
-        public mixed  $title       = null,
-        public mixed  $description = null,
-        public mixed  $actions     = null,
-        public bool   $horizontal  = false,
-        public bool   $separators  = false
+        public mixed   $figure      = null,
+        public mixed   $title       = null,
+        public mixed   $description = null,
+        public mixed   $actions     = null,
+        public bool    $horizontal  = false,
+        public bool    $separators  = false,
+        public ?string $color       = null,
+        public ?string $size        = null,
     ) {
     }
 
@@ -22,8 +24,22 @@ class Card extends Component
         return <<<'HTML'
         <div {{
             $attributes->class([
-                'card bg-base-100 shadow-xl',
-                'card-side' => $horizontal
+                'card border-1',
+                'card-side' => $horizontal,
+                'border-base-300'  => $color === null,
+                'border-neutral'   => $color === 'neutral',
+                'border-primary'   => $color === 'primary',
+                'border-secondary' => $color === 'secondary',
+                'border-accent'    => $color === 'accent',
+                'border-info'      => $color === 'info',
+                'border-success'   => $color === 'success',
+                'border-warning'   => $color === 'warning',
+                'border-error'     => $color === 'error',
+                'card-xs'          => $size === "xs",
+                'card-sm'          => $size === "sm",
+                'card-md'          => $size === "md",
+                'card-lg'          => $size === "lg",
+                'card-xl'          => $size === "xl",
             ])->merge()
         }}> 
             @if ($figure)
@@ -59,7 +75,17 @@ class Card extends Component
                     @if ($separators)
                         <hr class="mt-3 border-t-[length:var(--border)] border-base-content/10">
                     @endif
-                    <div class="card-actions justify-end">
+                    <div @class([
+                        'card-actions justify-end',
+                        'ayn-desc:[:where(button.btn)]:btn-neutral ayn-desc:[:where(button.btn)]:text-neutral-content'     => $color === 'neutral',
+                        'ayn-desc:[:where(button.btn)]:btn-primary ayn-desc:[:where(button.btn)]:text-primary-content'     => $color === 'primary',
+                        'ayn-desc:[:where(button.btn)]:btn-secondary ayn-desc:[:where(button.btn)]:text-secondary-content' => $color === 'secondary',
+                        'ayn-desc:[:where(button.btn)]:btn-accent ayn-desc:[:where(button.btn)]:text-accent-content'       => $color === 'accent',
+                        'ayn-desc:[:where(button.btn)]:btn-info ayn-desc:[:where(button.btn)]:text-info-content'           => $color === 'info',
+                        'ayn-desc:[:where(button.btn)]:btn-success ayn-desc:[:where(button.btn)]:text-success-content'     => $color === 'success',
+                        'ayn-desc:[:where(button.btn)]:btn-warning ayn-desc:[:where(button.btn)]:text-warning-content'     => $color === 'warning',
+                        'ayn-desc:[:where(button.btn)]:btn-error ayn-desc:[:where(button.btn)]:text-error-content'         => $color === 'error',
+                    ])>
                         {{ $actions }}
                     </div>
                 @endif
