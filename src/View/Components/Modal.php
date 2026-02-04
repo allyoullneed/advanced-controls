eb4ee7af0f7b5b3de80df9887f15cd100766c297
FX David <fxavierdavid@gmail.com>
2026-02-04 22:23:23 +0100

    Forced placement of modal

--- src/View/Components/Modal.php
+++ src/View/Components/Modal.php
@@ -19,7 +19,7 @@ class Modal extends Component
     public function render(): View|Closure|string
     {
         return <<<'HTML'
-        <dialog id="{{ $id }}" class="fixed max-w-none w-[100vw] max-h-none h-[100vh] place-items-center hidden open:grid bg-transparent open:bg-[#0006]">
+        <dialog id="{{ $id }}" class="fixed left-0 top-0 max-w-none w-[100vw] max-h-none h-[100vh] place-items-center hidden open:grid bg-transparent open:bg-[#0006]">
             {{ $slot }}
         </dialog>
         HTML;
