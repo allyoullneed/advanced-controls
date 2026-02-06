40427e77c76a56ce7cf5fd2d4ef7ea250b910840
FX David <fxavierdavid@gmail.com>
2026-02-06 17:00:10 +0100

    Merging attributes for Modal

--- src/View/Components/Modal.php
+++ src/View/Components/Modal.php
@@ -19,7 +19,11 @@ class Modal extends Component
     public function render(): View|Closure|string
     {
         return <<<'HTML'
-        <dialog id="{{ $id }}" class="fixed left-0 top-0 max-w-none w-[100vw] max-h-none h-[100vh] place-items-center hidden open:grid bg-transparent open:bg-[#0006]">
+        <dialog id="{{ $id }}"
+        {{ $attributes->class([
+            'fixed left-0 top-0 max-w-none w-[100vw] max-h-none h-[100vh] place-items-center hidden open:grid bg-transparent open:bg-[#0006] backdrop-blur-xl'
+            ])->merge()
+        }}>
             {{ $slot }}
         </dialog>
         HTML;
