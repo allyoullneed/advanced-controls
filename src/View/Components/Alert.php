5d4c2903b59d65344bddd6dddd8e02daf7aec239
FX David <fxavierdavid@gmail.com>
2026-02-19 21:19:21 +0100

    Alert: Fixed support for FlyonUI

--- src/View/Components/Alert.php
+++ src/View/Components/Alert.php
@@ -26,8 +26,7 @@ class Alert extends Component
         return <<<'HTML'
         <div
             {{ $attributes->class([
-                'alert pb-0',
-                'flex gap-4'             => $icon !== '',
+                'alert grid grid-cols-1 justify-start gap-y-3',
                 'alert-primary'          => ($type ?? $color) === 'primary',
                 'alert-info'             => ($type ?? $color) === 'info',
                 'alert-success'          => ($type ?? $color) === 'success',
@@ -37,28 +36,41 @@ class Alert extends Component
                 'alert-outline'          => $variant === 'outline',
                 'alert-dash alert-outline border-dashed' => $variant === 'dash'
                 ])->merge() }}
-            role="alert"
-        >
+            role="alert">
+            <div
+                @class([
+                'alert grid-cols-1 p-0 border-none bg-transparent shadow-none',
+                'alert-primary'          => ($type ?? $color) === 'primary',
+                'alert-info'             => ($type ?? $color) === 'info',
+                'alert-success'          => ($type ?? $color) === 'success',
+                'alert-warning'          => ($type ?? $color) === 'warning',
+                'alert-error'            => ($type ?? $color) === 'error',
+                'alert-soft'             => $variant === 'soft' || $variant === 'ghost',
+                'alert-outline'          => $variant === 'outline',
+                'alert-dash alert-outline border-dashed' => $variant === 'dash'
+                ])
+            >
+            <div class="flex items-start gap-4">
             @if (isset($icon) && $icon !== "")
                 @if (gettype($icon) === 'string')
-                    <x-icon :name="$icon" class="mb-4 shrink-0 size-6"/>
+                    <x-icon :name="$icon" class="shrink-0 size-6"/>
                 @else
                     <div
-                        {{ $icon->attributes->merge(['class' => 'mb-4']) }}
+                        {{ $icon->attributes}}
                     >
                     {{ $icon }}
                     </div>
                 @endif
             @elseif ($type === 'success')
-                <x-icon name="heroicon-o-check-circle" class="mb-4 shrink-0 size-6"/>
+                <x-icon name="heroicon-o-check-circle" class="shrink-0 size-6"/>
             @elseif ($type === 'warning')
-                <x-icon name="heroicon-o-exclamation-triangle" class="mb-4 shrink-0 size-6"/>
+                <x-icon name="heroicon-o-exclamation-triangle" class="shrink-0 size-6"/>
             @elseif ($type === 'error')
-                <x-icon name="heroicon-o-x-circle" class="mb-4 shrink-0 size-6"/>
+                <x-icon name="heroicon-o-x-circle" class="shrink-0 size-6"/>
             @elseif (($type === "" || $type === "info") && $icon === null)
-                <x-icon name="heroicon-o-information-circle" class="mb-4 shrink-0 size-6"/>
+                <x-icon name="heroicon-o-information-circle" class="shrink-0 size-6"/>
             @endif
-            <div class="basis-full mb-4 block">
+            <div class="basis-fullblock self-center flex flex-col items-start">
                 @if ($title)
                     @if (gettype($title) === 'object')
                         <span {{
@@ -68,14 +80,14 @@ class Alert extends Component
                         }}>{{ $title }}</span>
                     @else
                         <span {{
-                            $attributes->class([
+                            $attributes->except(['class'])->class([
                                 'block font-bold' => $description != null
                             ])
                         }}>{{ $title }}</span>
                     @endif
                 @endif
                     @if (gettype($description) === 'object' && $slot->isNotEmpty())
-                        <span {{ $attributes->class(['text-xs' => $title != null])->merge() }}>
+                        <span {{ $attributes->except(['class'])->class(['text-xs' => $title != null])->merge() }}>
                             <span {{ $description->attributes->class(['text-xs' => $title != null])->merge() }}>
                                 {{ $description }}
                             </span>
@@ -86,23 +98,24 @@ class Alert extends Component
                             {{ $description }}
                         </span>
                     @elseif ($description || $slot->isNotEmpty())
-                        <span {{ $attributes->class(['text-xs' => $title != null])->merge() }}>
+                        <span {{ $attributes->except(['class'])->class(['text-xs' => $title != null])->merge() }}>
                             {{ $description }}
                             {{ $slot }}
                         </span>
                         @endif
 
             </div>
+            </div>
+            </div>
             @if (isset($actions))
                 <div 
-                    {{ $actions->attributes->class([
-                        'flex gap-2 mb-4 justify-self-end ayn-child:[:where(.btn)]:btn-sm'
+                    {{ $actions->attributes->except(['class'])->class([
+                        'ayn-child:[:where(.btn)]:btn-sm'
                     ])->merge() }}
                 >
                 {{ $actions }}
                 </div>
             @endif
-
         </div>
         HTML;
     }
